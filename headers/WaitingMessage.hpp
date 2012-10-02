a5b276d32c136a0c562190be53f70675c756daf9
Quentin Geissmann <qgeissmann@gmail.com>
2012-10-02 20:57:52 +0100

    add the files related to the last commit

---
+++ headers/WaitingMessage.hpp
@@ -0,0 +1,17 @@
+#ifndef WAITINGMESSAGE_H
+#define WAITINGMESSAGE_H
+#include <gtkmm.h>
+
+class WaitingMessage : public Gtk::Window
+{
+    public:
+        WaitingMessage();
+        virtual ~WaitingMessage();
+        void runMe();
+
+    protected:
+    private:
+    Gtk::Label m_label_message;
+};
+
+#endif // WAITINGMESSAGE_H
---
+++ src/WaitingMessage.cpp
@@ -0,0 +1,29 @@
+#include "WaitingMessage.hpp"
+#include "defines.hpp"
+
+WaitingMessage::WaitingMessage():
+m_label_message("Please wait, parameters are setting up. ")
+{
+
+#if defined WIN32 || defined WIN64
+this->set_icon_from_file("../ubitrail.ico");
+m_controlPanel->set_icon_from_file("../ubitrail.ico");
+#endif
+this->set_title(PROGRAM_NAME " - Setting up...");
+this->add(m_label_message);
+
+//            Glib::Thread messageThread;
+
+//
+this->show();
+this->show_all_children();
+}
+
+WaitingMessage::~WaitingMessage()
+{
+    //dtor
+}
+
+void WaitingMessage::runMe(){
+    Gtk::Main::run(*this);
+}
