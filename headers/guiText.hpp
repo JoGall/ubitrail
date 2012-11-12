acf42ed1450c5edf061a11dd308b890e4de8f55f
Joe Gallagher <boa07jg@sheffield.ac.uk>
2012-11-12 16:07:40 +0000

    updated guiText and added guiToolTips

--- headers/guiText.hpp
+++ headers/guiText.hpp
@@ -5,35 +5,34 @@
 
 #define PAGE_VIDEO_INPUT_TITLE "Video Input"
 #define PAGE_VIDEO_INPUT_DESCRIPTION "Select a .avi video file to analyse.  Most common video formats are \n\
-compatible (a full list is available at http:\/\/opencv.willowgarage.com\/wiki\/VideoCodecs).  The K-Lite \n\
-codec pack (available at http:\/\/codecguide.com\/) can be a useful tool to update your system codecs."
+compatible (a full list is available at http:\/\/opencv.willowgarage.com\/wiki\/VideoCodecs). \n\
+The K-Lite codec pack (available at http:\/\/codecguide.com\/) can be a useful tool to \n\
+update your system codecs."
 #define PAGE_VIDEO_INPUT_LOAD_FILE_BUTTON "Pick a file"
 #define PAGE_VIDEO_INPUT_DESCRIPTION_NO_FILE "No file selected yet"
+#define LINK_USER_MANUAL_COMPATABILITY
 
-#define PAGE_AREA_TITLE "Experimental Definition"
+#define PAGE_AREA_TITLE "Arena Definition"
 
-#define PAGE_AREA_DESCRIPTION "Define the details of the experimental design used in the selected video."
+#define PAGE_AREA_DESCRIPTION "Define the experimental arena."
 
-#define PAGE_AREA_DEFAULT_MODE_DESCRIPTION "By default, the experimental area remains undefined.\n\
-Use if your experiment contains only one area of interest."
-#define PAGE_AREA_AUTOCIRCLE_MODE_DESCRIPTION "Define the number of areas in the experiment in order to generate a mask.\n\
-If applicable, define the number of territories composing each area. The mask can then be viewed using \n\
-'Generate Preview', and can be saved for future use."
-#define PAGE_AREA_FILE_MODE_DESCRIPTION "Select a pre-existing mask for use with the current video."
-#define PAGE_AREA_MANUAL_MODE_DESCRIPTION "Manually define the quantities and dimensions of all areas and \n\
-territories within the experiment."
+#define PAGE_AREA_DEFAULT_MODE_DESCRIPTION "By default, the arena remains undefined.\n\
+Use this option if your arena contains only one area of interest with no subdivisions."
+#define PAGE_AREA_AUTOCIRCLE_MODE_DESCRIPTION "Automatically generates a mask based upon the defined number of \n\
+circular areas and comprising territories."
+#define PAGE_AREA_FILE_MODE_DESCRIPTION "Select a pre-existing mask for use with the current video, or save the \n\
+first frame of the current video in order to generate your own mask.  The UbiTrail \n\
+User Manual provides detailed information on how to make your own mask."
+#define PAGE_AREA_MANUAL_MODE_DESCRIPTION   "TODO"
+#define LINK_USER_MANUAL_MANUAL_MASK_DEFINITION
 
 #define PAGE_PROCPARS_TITLE "Processing Parameters"
 
-#define PAGE_PROCPARS_DESCRIPTION "Modify the parameters of the analysis. \n\
-Sensitivity determines how likely noise is to be detected as motion. It accepts \n\
-values from 0 to 255, and is usually most effective between the range of 2 to 10. \n\
-Low values may erroneously detect motion in pixelated noise, while extremely high \n\
-values may be overly insensitive and fail to detect motion at all. \n\
-The number of frames used to train the motion detector can also be defined."
+#define PAGE_PROCPARS_DESCRIPTION "Modify the parameters of the analysis."
 
 #define PAGE_OUTPUT_TITLE "Result Folder"
 #define PAGE_OUTPUT_DESCRIPTION "Choose where to save the results of the analysis. Data is outputted \n\
-in a .csv format, and can be readily interpreted using an available R package."
+in a .csv format, and can be readily analysed using the including R package, Rubitrail."
 
-#define PAGE_OUTPUT_DESCRIPTION_NO_FILE " No folder selected yet"
+#define PAGE_OUTPUT_DESCRIPTION_NO_FILE "No folder selected yet"
+#define LINK_USER_MANUAL_DATA_ANALYSIS
---
+++ headers/guiToolTips.hpp
@@ -0,0 +1,19 @@
+#ifndef GUITOOLTIPS_HPP_INCLUDED
+#define GUITOOLTIPS_HPP_INCLUDED
+
+#define TOOLTIP_AREA_AUTOCIRCLE_CIRCLES "Specifies the number of circular areas within the experimental arena"
+
+#define TOOLTIP_AREA_AUTOCIRCLE_LINES "Specifies the number of territories composing each area \n\
+(default = 1 ; i.e. one territory per area)."
+
+
+#define TOOLTIP_PROCPARS_SENSITIVITY "Determines how likely noise is to be detected as motion. \n\
+Accepts values from 0 to 255, but is usually most effective between \n\
+the range 2-10.  Low values may erroneously detect motion in \n\
+pixelated noise, while extremely high values may be overly insensitive \n\
+and fail to detect motion at all."
+
+#define TOOLTIP_PROCPARS_THRESHOLD "The number of frames used to train the motion detector."
+
+
+#endif // GUITOOLTIPS_HPP_INCLUDED
